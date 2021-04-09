5ede1f08df956c9c76196c7a7af2b67308e665a0
onnylover <gudboy@naver.com>
2021-04-09 11:48:19 +0900

    python bs4 practive

---
+++ collection/crawler.py
@@ -0,0 +1,7 @@
+
+
+def crawling():
+    pass
+
+
+
---
+++ test/test_bs4.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+
+html = """
+<td class="title">
+	<div class="tit3">
+		<a href="/movie/bi/mi/basic.nhn?code=189075" title="자산어보">자산어보</a>
+	</div>
+</td>"""
+
+
+# 1. tag 조회
+def ex01():
+    # 대상 다음에 어떤 구문인지 인식하게 해줘야함
+    bs = BeautifulSoup(html,"html.parser")
+
+    # 특정 장소로 지정할 수 있음
+    tag_td = bs.td
+    # print(tag_td)
+    # tag_a = bs.a
+    # 상세화 시킬수도 있음
+    tag_a = tag_td.a
+    print(tag_a)
+
+    # None
+    tag_h1 = bs.td.h1
+    print(tag_h1)
+# 2. attribute 조회
+def ex02():
+    pass
+
+if __name__ == '__main__':
+    # ex01()
+    ex02()
